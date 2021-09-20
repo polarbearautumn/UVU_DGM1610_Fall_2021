87fce72bb9ecc95d82109b3e2e49e6a64f137575
Autumn Jones <polarbearautumn@gmail.com>
2021-09-20 12:10:35 -0600

    Add Main Player

---
+++ UVU_DGM1610_Fall_2021/Assets/MainPlayer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class MainPlayer : MonoBehaviour {
+
+    // Use this for initialization
+    void Start ()
+    {
+        Debug.Log("I am alive!");
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+}
