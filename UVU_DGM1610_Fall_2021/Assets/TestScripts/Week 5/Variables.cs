e2e4e2a558037f7f27353d4c26ae4cbef3672246
Autumn Jones <polarbearautumn@gmail.com>
2021-09-21 17:55:35 -0600

    Add Variables

---
+++ UVU_DGM1610_Fall_2021/Assets/TestScripts/Week 5/Variables.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour;
+
+string myName = "Autumn Jones";
+Debug.Log(myName);
+
+char myInitial = 'A';
+Debug.Log(myInitial);
+
+int myAge = 20;
+Debug.Log(myAge);
+
+float myExactAge = 19.9F;
+Debug.Log(myExactAge);
+
+float myHeight = 5.3F;
+myHeight = 6.3F;
+Debug.Log(myHeight);
+
+double myExactHeight = 5.3999999D;
+Debug.Log(myExactHeight);
+
+long myStudentLoanDebt;
+myStudentLoanDebt = 100000000L;
+Debug.Log(myStudentLoanDebt);
+
+bool myBool = true;
+Debug.Log(myBool);
+
+
+
+
+
