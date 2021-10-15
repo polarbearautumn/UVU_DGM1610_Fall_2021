9d5894356b7cdc69939f21797ad020b5d0956e00
Autumn Jones <polarbearautumn@gmail.com>
2021-10-14 18:06:51 -0600

    Add PlayerController Script

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Public variables
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Stop player from leaving left side of screen
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        
+        // Stop player from leaving right side of screen
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        
+        // Move player along x axis with arrow keys
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
