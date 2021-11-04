89028dd8498d07fbc0c0b10d4bd138d5d109cb4e
Autumn Jones <polarbearautumn@gmail.com>
2021-11-03 23:49:45 -0600

    Clean Scripts

--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    // Public and private variables
+    public GameObject enemyPrefab;
+    private float spawnRange = 9.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Spawns an enemy prefab in a random location and a specified rotation
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Generates a random spawn position (in this case, for an enemy)
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
     }
 }
