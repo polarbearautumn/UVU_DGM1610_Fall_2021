61dfc731d8e8b48fa1a5738a468dc0d92497ed9a
Autumn <polarbearautumn@gmail.com>
2021-11-09 14:46:48 -0700

    Add BasicMovement Script

---
+++ Personal Project/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    // Public and private variables
+    public float speed = 2.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Translate(Vector3.down * speed * Time.deltaTime); 
+    }
+}
