9caef3607f5cea2fcdc713f5bc6bf5fb0b5c9300
Autumn Jones <polarbearautumn@gmail.com>
2021-11-12 20:13:09 -0700

    Optimize Prototype 2 Project

--- Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -1,31 +1,35 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    // Private variables
-    private float topBound = 30.0f;
-    private float lowerBound = -10.0f;
-    
+    private float topBound = 30;
+    private float lowerBound = -10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Destroys gameObjects that leave the screen
         if (transform.position.z > topBound)
         {
-            Destroy(gameObject);
+            // Instead of destroying the projectile when it leaves the screen
+            //Destroy(gameObject);
+
+            // Just deactivate it
+            gameObject.SetActive(false);
+
         }
         else if (transform.position.z < lowerBound)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
         }
+
     }
 }
